bb234d32494a09db0841954737e9f38a0dbe3b6e
Raul Castro Palmett <rcastrop@outlook.com>
2025-06-21 12:50:53 -0500

    Ajuste

---
+++ start-env.sh
@@ -0,0 +1,22 @@
+python3.11 -m venv mindsdb-venv
+source mindsdb-venv/bin/activate
+pip install --upgrade pip setuptools wheel
+pip install -e .
+pip install -r requirements/requirements-dev.txt
+python -m mindsdb --api http,mysql,mongodb,postgres,mcp --config mindsdb_config.json
+# cd mindsdb
+# pip install .[handler_name]
+
+export MINDSDB_USERNAME=root
+export MINDSDB_PASSWORD=EticosLtda2025*
+
+
+unset MINDSDB_USERNAME
+unset MINDSDB_PASSWORD
+
+
+
+python -m mindsdb --api mysql,mongodb,postgres,mcp --config mindsdb_config.json
+
+
+python -m mindsdb --api mysql --config mindsdb_config.json
