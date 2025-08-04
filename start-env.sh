a25e7b08e660cd75b32d42d0dbf1a39e62620b3a
Raul Castro Palmett <rcastrop@outlook.com>
2025-08-04 09:15:38 -0500

    Actualizacion a uv

--- start-env.sh
+++ start-env.sh
@@ -20,10 +20,20 @@ unset MINDSDB_PASSWORD
 python -m mindsdb --api mysql,mongodb,postgres,mcp --config mindsdb_config.json
 
 
-python -m mindsdb --api mysql --config mindsdb_config.json
+python -m mindsdb --api mysql,http --config mindsdb_config.json
 
 python -m mindsdb --api mysql,http --config mindsdb_config.json
 
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
+
+# Ultima version of MindsDB NegExAI
+uv venv --python 3.12
+source .venv/bin/activate
+uv pip install --upgrade pip setuptools wheel
+uv pip install --prerelease=allow -e .
+uv pip install -r requirements/requirements.txt
+
+cd ../mindsdb_sql_parser
+uv pip install -e .
